5a7c836ec503f90c3f04f853a006af0faef46f4b
Arhat161 <Arhat161@users.noreply.github.com>
2022-08-22 20:05:42 +0300

    updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,39 @@
+import java.sql.*;
+
+import static java.lang.Class.forName;
+
 public class Main {
+
+
+
     public static void main(String[] args) {
-        String url, username, password;
-        
+
+        String jdbcDriver = "jdbc:postgresql";
+        String host = "localhost";
+        String port = "5432";
+        String targetDatabase = "first_db";
+        String url = jdbcDriver + "://" + host + ":" + port + "/" + targetDatabase;
+        String username = "postgres";
+        String password = "123";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            PreparedStatement pr = connection.prepareStatement("SELECT * FROM product");
+            ResultSet rs = pr.executeQuery();
+
+            while(rs.next()) {
+                int product_id = rs.getInt(1);
+                String product_name = rs.getString(2);
+                int product_price = rs.getInt(3);
+                System.out.println(product_id + " | " + product_name + " | " + product_price);
+            }
+            // close all
+            rs.close();
+            pr.close();
+            connection.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
